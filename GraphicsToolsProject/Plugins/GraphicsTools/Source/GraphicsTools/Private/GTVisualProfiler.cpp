2c50222f95ea68422ab06a7df0b5a8dec6815208
Cameron Micka <thmicka@microsoft.com>
2021-02-09 16:22:06 -0800

    Wip frame timings.

--- GraphicsToolsProject/Plugins/GraphicsTools/Source/GraphicsTools/Private/GTVisualProfiler.cpp
+++ GraphicsToolsProject/Plugins/GraphicsTools/Source/GraphicsTools/Private/GTVisualProfiler.cpp
@@ -10,52 +10,79 @@
 #include "Materials/MaterialInstanceDynamic.h"
 #include "UObject/ConstructorHelpers.h"
 
+const int32 NumberOfSamples = 200;
+
 AGTVisualProfiler::AGTVisualProfiler()
+	: RenderThreadTime(0.0f)
+	, GameThreadTime(0.0f)
+	, GPUFrameTime{0.0f}
+	, FrameTime(0.0f)
+	, RawRenderThreadTime(0.0f)
+	, RawGameThreadTime(0.0f)
+	, RawGPUFrameTime{0.0f}
+	, RawFrameTime(0.0f)
+//, CurrentIndex(0)
 {
+	// RenderThreadTimes.AddZeroed(NumberOfSamples);
+	// GameThreadTimes.AddZeroed(NumberOfSamples);
+	// for (auto& GPUFrameTimesArray : GPUFrameTimes)
+	//{
+	//	GPUFrameTimesArray.AddZeroed(NumberOfSamples);
+	//}
+	// FrameTimes.AddZeroed(NumberOfSamples);
+
 	PrimaryActorTick.bCanEverTick = true;
 
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> PlaneMesh(TEXT("/Engine/BasicShapes/Plane"));
-	check(PlaneMesh.Object);
+	// Acquire default materials.
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> DefaultQuadMeshFinder(TEXT("/Engine/BasicShapes/Plane"));
+	check(DefaultQuadMeshFinder.Object);
+	DefaultQuadMesh = DefaultQuadMeshFinder.Object;
 
-	static ConstructorHelpers::FObjectFinder<UMaterial> DefaultMaterial(
+	static ConstructorHelpers::FObjectFinder<UMaterial> DefaultMaterialFinder(
 		TEXT("Material'/Engine/EngineDebugMaterials/LevelColorationUnlitMaterial.LevelColorationUnlitMaterial'"));
-	check(DefaultMaterial.Object);
+	check(DefaultMaterialFinder.Object);
+	DefaultMaterial = DefaultMaterialFinder.Object;
 
-	static ConstructorHelpers::FObjectFinder<UMaterial> DefaultTextMaterial(TEXT("Material'/Engine/EngineMaterials/UnlitText.UnlitText'"));
-	check(DefaultTextMaterial.Object);
+	static ConstructorHelpers::FObjectFinder<UMaterial> DefaultTextMaterialFinder(
+		TEXT("Material'/Engine/EngineMaterials/UnlitText.UnlitText'"));
+	check(DefaultTextMaterialFinder.Object);
+	DefaultTextMaterial = DefaultTextMaterialFinder.Object;
 
+	// Create child components.
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("VisualProfiler"));
 
+	BackPlate = CreateQuad(
+		TEXT("BackPlate"), RootComponent, FVector::ZeroVector, FRotator(0, 90, 90), FVector(0.1f, 0.02f, 1),
+		FLinearColor(20 / 256.0f, 20 / 256.0f, 20 / 256.0f, 1));
+
+	const float DepthOffset = -0.02f;
+	const FRotator TextRotation(0, 180, 0);
+	const FString ZeroMs(TEXT("0.00 ms"));
 	{
-		BackPlate = CreateAndAttachComponent<UStaticMeshComponent>(TEXT("BackPlate"), RootComponent);
-		BackPlate->SetRelativeRotation(FRotator(0, 90, 90));
-		BackPlate->SetRelativeScale3D(FVector(0.1f, 0.02f, 1));
-		BackPlate->SetStaticMesh(PlaneMesh.Object);
-		UMaterialInstanceDynamic* Material = BackPlate->CreateDynamicMaterialInstance(0, DefaultMaterial.Object);
-		Material->SetVectorParameterValue(TEXT("Color"), FLinearColor(20 / 256.0f, 20 / 256.0f, 20 / 256.0f, 1));
-		BackPlate->SetMaterial(0, Material);
-		BackPlate->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		FrameTimeLabelPrefix =
+			CreateText(TEXT("FrameTimeLabelPrefix"), RootComponent, FVector(DepthOffset, -4.7f, 0.6f), TextRotation, TEXT("Frame: "));
+		FrameTimeLabel = CreateText(TEXT("FrameTimeLabel"), RootComponent, FVector(DepthOffset, -3.3f, 0.6f), TextRotation, ZeroMs);
+
+		GameThreadTimeLabelPrefix =
+			CreateText(TEXT("GameThreadTimeLabelPrefix"), RootComponent, FVector(DepthOffset, -4.7f, 0.2f), TextRotation, TEXT("Game: "));
+		GameThreadTimeLabel =
+			CreateText(TEXT("GameThreadTimeLabel"), RootComponent, FVector(DepthOffset, -3.3f, 0.2f), TextRotation, ZeroMs);
+
+		RenderThreadTimeLabelPrefix = CreateText(
+			TEXT("RenderThreadTimeLabelPrefix"), RootComponent, FVector(DepthOffset, -4.7f, -0.2f), TextRotation, TEXT("Draw: "));
+		RenderThreadTimeLabel =
+			CreateText(TEXT("RenderThreadTimeLabel"), RootComponent, FVector(DepthOffset, -3.3f, -0.2f), TextRotation, ZeroMs);
+
+		GPUTimeLabelPrefix =
+			CreateText(TEXT("GPUTimeLabelPrefix"), RootComponent, FVector(DepthOffset, -4.7f, -0.6f), TextRotation, TEXT("GPU: "));
+		GPUTimeLabel = CreateText(TEXT("GPUTimeLabel"), RootComponent, FVector(DepthOffset, -3.3f, -0.6f), TextRotation, ZeroMs);
 	}
-	{
-		DrawCallsLabel = CreateAndAttachComponent<UTextRenderComponent>(TEXT("DrawCallsLabel"), RootComponent);
-		DrawCallsLabel->SetRelativeLocation(FVector(-0.1f, 0, 0.25));
-		DrawCallsLabel->SetRelativeRotation(FRotator(0, 180, 0));
-		DrawCallsLabel->SetMaterial(0, DefaultTextMaterial.Object);
-		DrawCallsLabel->SetWorldSize(0.5f);
-		DrawCallsLabel->SetHorizontalAlignment(EHorizTextAligment::EHTA_Center);
-		DrawCallsLabel->SetVerticalAlignment(EVerticalTextAligment::EVRTA_TextCenter);
-		DrawCallsLabel->SetText(FText::AsCultureInvariant("Draw Calls: 0"));
-	}
-	{
-		PrimitivesLabel = CreateAndAttachComponent<UTextRenderComponent>(TEXT("PrimitivesLabel"), RootComponent);
-		PrimitivesLabel->SetRelativeLocation(FVector(-0.1f, 0, -0.25));
-		PrimitivesLabel->SetRelativeRotation(FRotator(0, 180, 0));
-		PrimitivesLabel->SetMaterial(0, DefaultTextMaterial.Object);
-		PrimitivesLabel->SetWorldSize(0.5f);
-		PrimitivesLabel->SetHorizontalAlignment(EHorizTextAligment::EHTA_Center);
-		PrimitivesLabel->SetVerticalAlignment(EVerticalTextAligment::EVRTA_TextCenter);
-		PrimitivesLabel->SetText(FText::AsCultureInvariant("Primitives: 0"));
-	}
+
+	DrawCallsLabel =
+		CreateText(TEXT("DrawCallsLabel"), RootComponent, FVector(DepthOffset, 0, 0.25), FRotator(0, 180, 0), TEXT("Draw Calls: 0"));
+
+	PrimitivesLabel =
+		CreateText(TEXT("PrimitivesLabel"), RootComponent, FVector(DepthOffset, 0, -0.25), FRotator(0, 180, 0), TEXT("Primitives: 0"));
 }
 
 void AGTVisualProfiler::BeginPlay()
@@ -67,19 +94,106 @@ void AGTVisualProfiler::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Draw calls
+	{
+		float DiffTime = FApp::GetCurrentTime() - FApp::GetLastTime();
+
+		RawFrameTime = DiffTime * 1000.0f;
+		FrameTime = 0.9 * FrameTime + 0.1 * RawFrameTime;
+
+		/** Number of milliseconds the gamethread was used last frame. */
+		RawGameThreadTime = FPlatformTime::ToMilliseconds(GGameThreadTime);
+		GameThreadTime = 0.9 * GameThreadTime + 0.1 * RawGameThreadTime;
+
+		/** Number of milliseconds the renderthread was used last frame. */
+		RawRenderThreadTime = FPlatformTime::ToMilliseconds(GRenderThreadTime);
+		RenderThreadTime = 0.9 * RenderThreadTime + 0.1 * RawRenderThreadTime;
+
+		for (uint32 GPUIndex : FRHIGPUMask::All())
+		{
+			/** Number of milliseconds the GPU was busy last frame. */
+			const uint32 GPUCycles = RHIGetGPUFrameCycles(GPUIndex);
+			RawGPUFrameTime[GPUIndex] = FPlatformTime::ToMilliseconds(GPUCycles);
+			GPUFrameTime[GPUIndex] = 0.9 * GPUFrameTime[GPUIndex] + 0.1 * RawGPUFrameTime[GPUIndex];
+		}
+
+		/*RenderThreadTimes[CurrentIndex] = RenderThreadTime;
+		GameThreadTimes[CurrentIndex] =GameThreadTime;
+		for (uint32 GPUIndex : FRHIGPUMask::All())
+		{
+			GPUFrameTimes[GPUIndex][CurrentIndex] = GPUFrameTime[GPUIndex];
+		}
+		FrameTimes[CurrentIndex] = FrameTime;
+
+		CurrentIndex++;
+
+		if (CurrentIndex == NumberOfSamples)
+		{
+			CurrentIndex = 0;
+		}*/
+	}
+
+	// Timings.
+	{
+		FrameTimeLabel->SetText(FText::AsCultureInvariant(FString::Printf(TEXT("%3.2f ms"), FrameTime)));
+		GameThreadTimeLabel->SetText(FText::AsCultureInvariant(FString::Printf(TEXT("%3.2f ms"), GameThreadTime)));
+		RenderThreadTimeLabel->SetText(FText::AsCultureInvariant(FString::Printf(TEXT("%3.2f ms"), RenderThreadTime)));
+		GPUTimeLabel->SetText(FText::AsCultureInvariant(FString::Printf(TEXT("%3.2f ms"), GPUFrameTime[0])));
+	}
+
+	// Draw calls.
 	{
 		const int32 NumDrawCalls = GNumDrawCallsRHI;
 		DrawCallsLabel->SetText(FText::Format(FText::AsCultureInvariant("Draw Calls: {0}"), FText::AsNumber(NumDrawCalls)));
 	}
 
-	// Primitives
+	// Primitives.
 	{
 		int32 NumPrimitives = GNumPrimitivesDrawnRHI;
-		PrimitivesLabel->SetText(FText::Format(FText::AsCultureInvariant("Primitives: {0}"), FText::AsNumber(NumPrimitives)));
+
+		if (NumPrimitives < 10000)
+		{
+			PrimitivesLabel->SetText(FText::Format(FText::AsCultureInvariant("Primitives: {0}"), FText::AsNumber(NumPrimitives)));
+		}
+		else
+		{
+			// TODO
+			//float NumPrimitivesK = NumPrimitives / 1000.f;
+			//PrimitivesLabel->SetText(
+			//	FText::Format(FText::AsCultureInvariant("Primitives: {0}"), *FString::Printf(TEXT("%.1fK"), NumPrimitivesK)));
+		}
+
 	}
 
 	UpdateTransform(DeltaTime);
+}
+
+UStaticMeshComponent* AGTVisualProfiler::CreateQuad(
+	const FName& Name, USceneComponent* Parent, FVector RelativeLocation, FRotator RelativeRotation, FVector RelativeScale,
+	FLinearColor Color)
+{
+	UStaticMeshComponent* Component = CreateAndAttachComponent<UStaticMeshComponent>(Name, Parent, RelativeLocation, RelativeRotation);
+	Component->SetRelativeScale3D(RelativeScale);
+	Component->SetStaticMesh(DefaultQuadMesh);
+	UMaterialInstanceDynamic* Material = Component->CreateDynamicMaterialInstance(0, DefaultMaterial);
+	Material->SetVectorParameterValue(TEXT("Color"), Color);
+	Component->SetMaterial(0, Material);
+	Component->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	return Component;
+}
+
+UTextRenderComponent* AGTVisualProfiler::CreateText(
+	const FName& Name, USceneComponent* Parent, FVector RelativeLocation, FRotator RelativeRotation, const FString& Text, float Size,
+	bool LeftAlign)
+{
+	UTextRenderComponent* Component = CreateAndAttachComponent<UTextRenderComponent>(Name, Parent, RelativeLocation, RelativeRotation);
+	Component->SetMaterial(0, DefaultTextMaterial);
+	Component->SetWorldSize(Size);
+	Component->SetHorizontalAlignment(LeftAlign ? EHorizTextAligment::EHTA_Left : EHorizTextAligment::EHTA_Right);
+	Component->SetVerticalAlignment(EVerticalTextAligment::EVRTA_TextCenter);
+	Component->SetText(FText::AsCultureInvariant(Text));
+
+	return Component;
 }
 
 void AGTVisualProfiler::UpdateTransform(float DeltaTime)
--- GraphicsToolsProject/Plugins/GraphicsTools/Source/GraphicsTools/Public/GTVisualProfiler.h
+++ GraphicsToolsProject/Plugins/GraphicsTools/Source/GraphicsTools/Public/GTVisualProfiler.h
@@ -13,7 +13,7 @@ class UStaticMeshComponent;
 class UTextRenderComponent;
 
 /**
- * TODO
+ * TODO Timing calculations mirrored from FStatUnitData.
  */
 UCLASS(ClassGroup = GraphicsTools)
 class GRAPHICSTOOLS_API AGTVisualProfiler : public AActor
@@ -34,12 +34,21 @@ private:
 
 	/** Utility method to allocate and add a scene component to the profiler. */
 	template <class T>
-	T* CreateAndAttachComponent(FName Name, USceneComponent* Parent)
+	T* CreateAndAttachComponent(FName Name, USceneComponent* Parent, FVector RelativeLocation, FRotator RelativeRotation)
 	{
 		T* Component = CreateDefaultSubobject<T>(Name);
 		Component->SetupAttachment(Parent);
+		Component->SetRelativeLocation(RelativeLocation);
+		Component->SetRelativeRotation(RelativeRotation);
 		return Component;
 	}
+
+	UStaticMeshComponent* CreateQuad(
+		const FName& Name, USceneComponent* Parent, FVector RelativeLocation, FRotator RelativeRotation, FVector RelativeScale,
+		FLinearColor Color);
+	UTextRenderComponent* CreateText(
+		const FName& Name, USceneComponent* Parent, FVector RelativeLocation, FRotator RelativeRotation, const FString& Text,
+		float Size = 0.5f, bool LeftAlign = true);
 
 	void UpdateTransform(float DeltaTime);
 
@@ -50,9 +59,69 @@ protected: // TEMP
 
 	/** TODO. */
 	UPROPERTY(VisibleAnywhere)
+	UTextRenderComponent* FrameTimeLabelPrefix = nullptr;
+
+	/** TODO. */
+	UPROPERTY(VisibleAnywhere)
+	UTextRenderComponent* FrameTimeLabel = nullptr;
+
+	/** TODO. */
+	UPROPERTY(VisibleAnywhere)
+	UTextRenderComponent* GameThreadTimeLabelPrefix = nullptr;
+
+	/** TODO. */
+	UPROPERTY(VisibleAnywhere)
+	UTextRenderComponent* GameThreadTimeLabel = nullptr;
+
+	/** TODO. */
+	UPROPERTY(VisibleAnywhere)
+	UTextRenderComponent* RenderThreadTimeLabelPrefix = nullptr;
+
+	/** TODO. */
+	UPROPERTY(VisibleAnywhere)
+	UTextRenderComponent* RenderThreadTimeLabel = nullptr;
+
+	/** TODO. */
+	UPROPERTY(VisibleAnywhere)
+	UTextRenderComponent* GPUTimeLabelPrefix = nullptr;
+
+	/** TODO. */
+	UPROPERTY(VisibleAnywhere)
+	UTextRenderComponent* GPUTimeLabel = nullptr;
+
+	/** TODO. */
+	UPROPERTY(VisibleAnywhere)
 	UTextRenderComponent* DrawCallsLabel = nullptr;
 
 	/** TODO. */
 	UPROPERTY(VisibleAnywhere)
 	UTextRenderComponent* PrimitivesLabel = nullptr;
+
+private:
+	UPROPERTY(Transient)
+	UStaticMesh* DefaultQuadMesh = nullptr;
+
+	UPROPERTY(Transient)
+	UMaterial* DefaultMaterial = nullptr;
+
+	UPROPERTY(Transient)
+	UMaterial* DefaultTextMaterial = nullptr;
+
+	/** Unit frame times filtered with a simple running average */
+	float RenderThreadTime;
+	float GameThreadTime;
+	float GPUFrameTime[MAX_NUM_GPUS];
+	float FrameTime;
+
+	/** Raw equivalents of the above variables */
+	float RawRenderThreadTime;
+	float RawGameThreadTime;
+	float RawGPUFrameTime[MAX_NUM_GPUS];
+	float RawFrameTime;
+
+	// int32 CurrentIndex;
+	// TArray<float> RenderThreadTimes;
+	// TArray<float> GameThreadTimes;
+	// TArray<float> GPUFrameTimes[MAX_NUM_GPUS];
+	// TArray<float> FrameTimes;
 };
