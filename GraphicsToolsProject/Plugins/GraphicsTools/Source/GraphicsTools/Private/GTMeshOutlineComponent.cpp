85d9c15e94360b292b4c61293ea003b9336fbf1a
Cameron Micka <thmicka@microsoft.com>
2021-07-19 16:17:46 -0700

    Wip, normal smoothing support.

--- GraphicsToolsProject/Plugins/GraphicsTools/Source/GraphicsTools/Private/GTMeshOutlineComponent.cpp
+++ GraphicsToolsProject/Plugins/GraphicsTools/Source/GraphicsTools/Private/GTMeshOutlineComponent.cpp
@@ -8,6 +8,7 @@ void UGTMeshOutlineComponent::PostLoad()
 	Super::PostLoad();
 
 	UpdateMesh();
+	UpdateMaterial();
 }
 
 void UGTMeshOutlineComponent::SetStaticMesh(UStaticMesh* Mesh)
@@ -15,6 +16,33 @@ void UGTMeshOutlineComponent::SetStaticMesh(UStaticMesh* Mesh)
 	if (StaticMesh != Mesh)
 	{
 		StaticMesh = Mesh;
+
+		UpdateMesh();
+		UpdateMaterial();
+	}
+}
+
+void UGTMeshOutlineComponent::SetOutlineThickness(float Thickness)
+{
+	if (OutlineThickness != Thickness)
+	{
+		UpdateMaterial();
+	}
+}
+
+void UGTMeshOutlineComponent::SetOutlineColor(FColor Color)
+{
+	if (OutlineColor != Color)
+	{
+		UpdateMaterial();
+	}
+}
+
+void UGTMeshOutlineComponent::SetComputeSmoothNormals(bool Compute)
+{
+	if (bComputeSmoothNormals != Compute)
+	{
+		bComputeSmoothNormals = Compute;
 
 		UpdateMesh();
 	}
@@ -26,6 +54,19 @@ void UGTMeshOutlineComponent::PostEditChangeProperty(FPropertyChangedEvent& Prop
 	Super::PostEditChangeProperty(PropertyChangedEvent);
 
 	if (PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED(UGTMeshOutlineComponent, StaticMesh))
+	{
+		UpdateMesh();
+		UpdateMaterial();
+	}
+	else if (PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED(UGTMeshOutlineComponent, OutlineThickness))
+	{
+		UpdateMaterial();
+	}
+	else if (PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED(UGTMeshOutlineComponent, OutlineColor))
+	{
+		UpdateMaterial();
+	}
+	else if (PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED(UGTMeshOutlineComponent, bComputeSmoothNormals))
 	{
 		UpdateMesh();
 	}
@@ -109,8 +150,85 @@ void UGTMeshOutlineComponent::UpdateMesh()
 		}
 	}
 
-	// Smooth the normals.
-	// TODO
+	if (bComputeSmoothNormals)
+	{
+		SmoothNormals(Vertices, Normals, Tangents);
+	}
 
 	CreateMeshSection(0, Vertices, Triangles, Normals, UV0, VertexColors, Tangents, false);
+}
+
+typedef TPair<int32, FVector> IndexVertex;
+
+void UGTMeshOutlineComponent::SmoothNormals(const TArray<FVector>& Vertices, TArray<FVector>& Normals, TArray<FProcMeshTangent>& Tangents)
+{
+	TMap<FVector, TArray<IndexVertex>> GroupedVerticies;
+	GroupedVerticies.Reserve(Vertices.Num()); // Reserve for the worse case, each vertex is at a unique location.
+
+	// Group all vertices that share the same location in space.
+	{
+		const int32 Count = Vertices.Num();
+		for (int32 Index = 0; Index < Count; ++Index)
+		{
+			TArray<IndexVertex>& SmoothingGroup = GroupedVerticies.FindOrAdd(Vertices[Index]);
+			SmoothingGroup.Add(IndexVertex(Index, Vertices[Index]));
+		}
+	}
+
+	// If we didn't hit the degenerate case of each vertex is its own group (no vertices shared a location), average the normals/tangents of
+	// each group.
+	if (GroupedVerticies.Num() != Vertices.Num())
+	{
+		TArray<FVector> SmoothNormals(Normals);
+		TArray<FProcMeshTangent> SmoothTangents(Tangents);
+
+		for (auto& Pair : GroupedVerticies)
+		{
+			TArray<IndexVertex>& SmoothingGroup = Pair.Value;
+
+			// No need to smooth a group of one.
+			if (SmoothingGroup.Num() != 1)
+			{
+				// Average the normals and tangents.
+				FVector SmoothedNormal = FVector::ZeroVector;
+				FVector SmoothedTangent = FVector::ZeroVector;
+
+				for (auto& Vertex : SmoothingGroup)
+				{
+					SmoothedNormal += Normals[Vertex.Key];
+					SmoothedTangent += Tangents[Vertex.Key].TangentX;
+				}
+
+				SmoothedNormal.Normalize();
+				SmoothedTangent.Normalize();
+
+				for (auto& Vertex : SmoothingGroup)
+				{
+					SmoothNormals[Vertex.Key] = SmoothedNormal;
+					SmoothTangents[Vertex.Key] = FProcMeshTangent(SmoothedTangent, false);
+				}
+			}
+		}
+
+		Normals = SmoothNormals;
+		Tangents = SmoothTangents;
+	}
+}
+
+void UGTMeshOutlineComponent::UpdateMaterial()
+{
+	UMaterialInterface* Material = GetMaterial(0);
+
+	if (Material == nullptr)
+	{
+		return;
+	}
+
+	UMaterialInstanceDynamic* MaterialInstance = CreateDynamicMaterialInstance(0, Material);
+
+	static const FName OutlineThicknessName = "OutlineThickness";
+	MaterialInstance->SetScalarParameterValue(OutlineThicknessName, OutlineThickness);
+
+	static const FName OutlineColorName = "OutlineColor";
+	MaterialInstance->SetVectorParameterValue(OutlineColorName, OutlineColor);
 }
--- GraphicsToolsProject/Plugins/GraphicsTools/Source/GraphicsTools/Public/GTMeshOutlineComponent.h
+++ GraphicsToolsProject/Plugins/GraphicsTools/Source/GraphicsTools/Public/GTMeshOutlineComponent.h
@@ -30,6 +30,30 @@ public:
 	UFUNCTION(BlueprintSetter, Category = "Mesh Outline")
 	void SetStaticMesh(UStaticMesh* Mesh);
 
+	/** TODO */
+	UFUNCTION(BlueprintGetter, Category = "Mesh Outline")
+	float GetOutlineThickness() const { return OutlineThickness; }
+
+	/** TODO */
+	UFUNCTION(BlueprintSetter, Category = "Mesh Outline")
+	void SetOutlineThickness(float Thickness);
+
+	/** TODO */
+	UFUNCTION(BlueprintGetter, Category = "Mesh Outline")
+	FColor GetOutlineColor() const { return OutlineColor; }
+
+	/** TODO */
+	UFUNCTION(BlueprintSetter, Category = "Mesh Outline")
+	void SetOutlineColor(FColor Color);
+
+	/** TODO */
+	UFUNCTION(BlueprintGetter, Category = "Mesh Outline")
+	bool GetComputeSmoothNormals() const { return bComputeSmoothNormals; }
+
+	/** TODO */
+	UFUNCTION(BlueprintSetter, Category = "Mesh Outline")
+	void SetComputeSmoothNormals(bool Compute);
+
 protected:
 #if WITH_EDITOR
 	/** TODO */
@@ -40,7 +64,28 @@ private:
 	/** TODO */
 	void UpdateMesh();
 
+	/** TODO */
+	void SmoothNormals(const TArray<FVector>& Vertices, TArray<FVector>& Normals, TArray<FProcMeshTangent>& Tangents);
+
+	/** TODO */
+	void UpdateMaterial();
+
 	/** TODO*/
 	UPROPERTY(EditAnywhere, BlueprintGetter = "GetStaticMesh", BlueprintSetter = "SetStaticMesh", Category = "Mesh Outline")
 	UStaticMesh* StaticMesh = nullptr;
+
+	/** TODO*/
+	UPROPERTY(
+		EditAnywhere, BlueprintGetter = "GetOutlineThickness", BlueprintSetter = "SetOutlineThickness", Category = "Mesh Outline",
+		Category = "Mesh Outline", meta = (UIMin = "0.0", UIMax = "10.0"))
+	float OutlineThickness = 0.5f;
+
+	/** TODO*/
+	UPROPERTY(EditAnywhere, BlueprintGetter = "GetOutlineColor", BlueprintSetter = "SetOutlineColor")
+	FColor OutlineColor = FColor(255, 0, 0, 255);
+
+	/** TODO*/
+	UPROPERTY(
+		EditAnywhere, BlueprintGetter = "GetComputeSmoothNormals", BlueprintSetter = "SetComputeSmoothNormals", Category = "Mesh Outline")
+	bool bComputeSmoothNormals = true;
 };
